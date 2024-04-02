f54427cfa32fddae7444db7bcf3544f4f481876f
Nick Petropoulos <Devilmanaltair1995@yahoo.com>
2024-04-02 21:29:17 +0300

    completed

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ const move_speed : float = 100.0
 const jump_force : float = 200.0
 const gravity : float = 500.0
 var score : int = 0
+@onready var score_text : Label = get_node("CanvasLayer/ScoreText")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -28,4 +29,5 @@ func game_over():
 	
 func add_score(ammount):
 	score += ammount
+	score_text.text = str("Score: ", score)
 	pass
